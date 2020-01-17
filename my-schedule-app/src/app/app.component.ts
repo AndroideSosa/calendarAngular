cc150b802effc959b8e26d3018cac64f540b2188
Andres Chavez Sosa <andres.chavezsosa1494@gmail.com>
2020-01-16 18:44:26 -0600

    Se cambian variables.
    Se hacen modificaciones dentro del controlador.

--- my-schedule-app/src/app/app.component.ts
+++ my-schedule-app/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   //Variables simulando estructura de servicios
-  public mesesArray = [
+  public monthArray = [
     { id:1,
       year:2020,
       name: "Enero",
@@ -51,59 +51,24 @@ export class AppComponent implements OnInit {
       previousMonth: "Diciembre",
       nextMonthId: "2",
       nextMonth: "Febrero"}, 
-    {
-      id:2,
-      year:2020,
-      name: "Febrero",
-      monday: [{id:1, day:'3', status: 'Active'},
-                {id:2, day:'10', status: 'Active'},
-                {id:3, day:'17', status: 'Active'},
-                {id:4, day:'17', status: 'Active'}],
-      tuesday: ['4','11','18','25'],
-      wednesday: ['5','12','19','26'],
-      thursday: ['6','13','20','27'],
-      friday: ['7','14','21','28'],
-      saturday: ['1','8','15','22','29'],
-      sunday: ['2','9','16','23'],
-      previousMonthId: "1",
-      previousMonth: "Enero",
-      nextMonthId: "3",
-      nextMonth: "Marzo"
-    }
   ];
 
-  public febreroArray = {
-    id:2,
-    year:2020,
-    name: "Febrero",
-    monday: ['3','10','17','24'],
-    tuesday: ['4','11','18','25'],
-    wednesday: ['5','12','19','26'],
-    thursday: ['6','13','20','27'],
-    friday: ['7','14','21','28'],
-    saturday: ['1','8','15','22','29'],
-    sunday: ['2','9','16','23'],
-    previousMonthId: "1",
-    previousMonth: "Enero",
-    nextMonthId: "3",
-    nextMonth: "Marzo"
-  };
   
-  public mondayArray = [];
-  public currentMonthArray = [];
+  public mondayArray: any = [];
+  public currentMonthArray: any =  [];
   public currentMonthTitle: string = null;
   public currentYearTitle: string = null;
   public previousMonthTitle: string = null;
   public nextMonthTitle: string = null;
 
   ngOnInit() {
-   console.log(this.mesesArray[0])
-   this.currentMonthArray = this.mesesArray[0];
+   console.log(this.monthArray[0])
+   this.currentMonthArray = this.monthArray[0];
    this.currentMonthTitle = this.currentMonthArray.name;
    this.currentYearTitle = this.currentMonthArray.year;
    this.previousMonthTitle = this.currentMonthArray.previousMonth;
    this.nextMonthTitle = this.currentMonthArray.nextMonth;
-   this.mondayArray = this.mesesArray[0].monday;
+   this.mondayArray = this.monthArray[0].monday;
    console.log(this.mondayArray);
   }
 
